fa556e1b97ea66039e9a3eb5694074ae8fc86791
elRealUa <161535328+elRealUa@users.noreply.github.com>
2024-03-26 18:13:03 -0500

    Add files via upload

---
+++ uaBot_2.py
@@ -0,0 +1,32 @@
+import discord
+import random
+import datetime
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+TOKEN = 'MTIxNzI1Njg1NDExMjE3ODIyNw.GC_Lb9.OBQdU17TEjS8csZJt60ahJdIOUVRgQnMBPq9X8'
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('-hola'):
+        await message.channel.send(f'Hola, soy {client.user}!, aqui está mi lista de comandos:')
+        await message.channel.send('-numero')
+        await message.channel.send('-hora')
+    elif  message.content.startswith('-numero'):
+        x = random.randint(-10000, 10000)
+        await message.channel.send(f'El número generado es: {x}')
+    elif message.content.startswith('-hora'):
+        hora = datetime.datetime.now()
+        horaA = hora.strftime('La hora actual es: %H: %M: %S')
+        await message.channel.send(horaA)
+
+        
+client.run(TOKEN)
